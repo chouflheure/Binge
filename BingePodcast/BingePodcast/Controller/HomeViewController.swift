ea92ba3b70cc0e697ec73ae2ee42c06302a3cd55
Charles Calvignac <charles.calvignac@neoxia.com>
2023-07-11 10:35:32 +0200

    Add shadow out side

--- BingePodcast/BingePodcast/Controller/HomeViewController.swift
+++ BingePodcast/BingePodcast/Controller/HomeViewController.swift
@@ -1,71 +1,17 @@
-//
-//  HomeViewController.swift
-//  BingePodcast
-//
-//  Created by charlesCalvignac on 03/07/2023.
-//
-
-/*
- let closeButton = CloseButton(action: { [weak self] in
-     self?.closeButtonAction()
- })
- 
- 
- 
- class CloseButton: UIButton {
-
-     private let action: () -> Void
-
-     init(action: @escaping () -> Void) {
-         self.action = action
-         super.init(frame: .zero)
-         setup()
-     }
-
-     @available(*, unavailable)
-     required init?(coder: NSCoder) {
-         fatalError("init(coder:) has not been implemented")
-     }
-
- }
-
-
- // MARK: - Setup
-
- private extension CloseButton {
-
-     func setup() {
-         addTarget(self, action: #selector(didTap), for: .touchUpInside)
-         let image = Assets.crossButton.image
-         accessibilityLabel = Strings.SportSkin.Accessibility.close
-
-         setBackgroundImage(image, for: .normal)
-         clipsToBounds = true
-         heightAnchor.constraint(equalToConstant: Constant.heightAnchor).isActive = true
-         widthAnchor.constraint(equalToConstant: Constant.widthAnchor).isActive = true
-     }
-
-     @objc private func didTap() {
-         action()
-     }
-
- }
- 
- */
-
 
 import UIKit
 
 class HomeViewController: UIViewController {
   
-    let molecule = SwitchPlayer()
-    let molecule2 = SwitchPlayer()
-    
-    let test = SlideToActionButton(title: "EPISODE 78", subtitle: "Cuisines indiennes, clichés en sauce, et ça pique", imageString: Assets.aBientotDeTeRevoir.name)
+    let molecule = SwitchPlayer(title: "EPISODE 78", subtitle: "Cuisines indiennes, clichés en sauce, et ça pique", imageString: Assets.aBientotDeTeRevoir.name)
+    let molecule2 = SwitchPlayer(title: "EPISODE 90", subtitle: "Cuisines indiennes", imageString: Assets.aBientotDeTeRevoir.name)
+
+ 
+ //   let test = SlideToActionButton(title: "EPISODE 78", subtitle: "Cuisines indiennes, clichés en sauce, et ça pique", imageString: Assets.aBientotDeTeRevoir.name)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+/*
         view.backgroundColor = .lightGray
         view.addSubview(test)
         test.translatesAutoresizingMaskIntoConstraints = false
@@ -76,11 +22,11 @@ class HomeViewController: UIViewController {
             test.heightAnchor.constraint(equalToConstant: 102),
             test.topAnchor.constraint(equalTo: view.topAnchor, constant: 300)
         ].forEach{$0.isActive = true}
-
-/*
-        let image = Assets.logo.image
-        let imageView = UIImageView(image: image)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+*/
+    
+    let image = Assets.logo.image
+    let imageView = UIImageView(image: image)
+    imageView.translatesAutoresizingMaskIntoConstraints = false
 
         view.addSubview(imageView)
         [
@@ -89,9 +35,7 @@ class HomeViewController: UIViewController {
             imageView.rightAnchor.constraint(equalTo: view.rightAnchor),
             imageView.heightAnchor.constraint(equalToConstant: 260)
         ].forEach{$0.isActive = true}
-        
-        
-        
+
         view.addSubview(molecule)
         molecule.translatesAutoresizingMaskIntoConstraints = false
         [
@@ -101,8 +45,7 @@ class HomeViewController: UIViewController {
             molecule.heightAnchor.constraint(equalToConstant: 102),
             molecule.topAnchor.constraint(equalTo: view.topAnchor, constant: 300)
         ].forEach{$0.isActive = true}
-        molecule.initSwitch()
-        
+
         view.addSubview(molecule2)
         molecule2.translatesAutoresizingMaskIntoConstraints = false
         [
@@ -112,10 +55,9 @@ class HomeViewController: UIViewController {
             molecule2.heightAnchor.constraint(equalToConstant: 102),
             molecule2.topAnchor.constraint(equalTo: view.topAnchor, constant: 450)
         ].forEach{$0.isActive = true}
-        molecule2.initSwitch()
-         
+
         setGradientBackground()
-*/
+
     }
     
     func setGradientBackground() {
--- BingePodcast/BingePodcast/Controller/molecule/SwitchPlayer.swift
+++ BingePodcast/BingePodcast/Controller/molecule/SwitchPlayer.swift
@@ -1,208 +1,52 @@
-//
-//  switchPlayer.swift
-//  BingePodcast
-//
-//  Created by charlesCalvignac on 04/07/2023.
-//
-
-import Foundation
 import UIKit
 
+protocol SlideToActionButtonDelegate: AnyObject {
+    func didFinish()
+}
+
 class SwitchPlayer: UIView {
-
-    var view = UIView()
-    let viewBase = UIView()
-    let imageRond = UIImageView()
-    let playerOnImage = UIImageView()
-    let text = UIStackView()
-    let title = UILabel()
-    let line = UIView()
-    let subtitle = UILabel()
-    let containerImageRond = UIView()
+    
     let innerShadowTop = CALayer()
+    var title = String()
+    var subtitle = String()
+    var imageString = String()
 
-    var isActive: Bool = false
-
-    func active() {
-        if isActive {
-            UIView.animate(withDuration: 0.8, delay: 0, options: [.curveEaseOut]) {
-                self.containerImageRond.frame.origin.x += UIScreen.main.bounds.width - 70 - 107
-                self.text.frame.origin.x -= 70
-                self.playerOnImage.image = Assets.Picto.pause.image
-                
-            }
-            self.innerShadowTop.isHidden = true
-        } else {
-            UIView.animate(withDuration: 0.5) {
-                self.containerImageRond.frame.origin.x -= UIScreen.main.bounds.width - 70 - 107
-                self.text.frame.origin.x += 70
-                self.playerOnImage.image = Assets.Picto.play.image
-                self.innerShadowTop.isHidden = false
-            }
-        }
-        
+    private var leftContainerImageCircleViewConstraint: NSLayoutConstraint?
+    private var leftTextStackViewConstraint: NSLayoutConstraint?
+    private var leftTextMinConstrait: CGFloat = 25
+    private var leftTextMaxConstrait: CGFloat = 105
+    
+    weak var delegate: SlideToActionButtonDelegate?
+    
+    private var xEndingPoint: CGFloat {
+        return (bounds.width - containerCircleImage.bounds.width - 7)
     }
+    
+    private var isFinished = false
+    
+    required init(title: String, subtitle: String, imageString: String) {
+        super.init(frame: .zero)
+        self.title = title
+        self.subtitle = subtitle
+        self.title = title
+        self.imageString = imageString
+        setup()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func layoutSubviews() {
         applyDesign()
     }
     
-    func initSwitch(/*view: UIView*/) {
-
-        // self.view = view
-        
-        backgroundColor = .white
-        layer.cornerRadius = 50
-        
-        
-        // Image Podcast
-        imageRond.image = Assets.aBientotDeTeRevoir.image
-        imageRond.translatesAutoresizingMaskIntoConstraints = false
-
-
-         
-        addSubview(containerImageRond)
-        containerImageRond.translatesAutoresizingMaskIntoConstraints = false
-        [
-            containerImageRond.widthAnchor.constraint(equalToConstant: 90),
-            containerImageRond.leftAnchor.constraint(equalTo: leftAnchor, constant: 7),
-            containerImageRond.heightAnchor.constraint(equalToConstant: 90),
-            containerImageRond.topAnchor.constraint(equalTo: topAnchor, constant: 7)
-        ].forEach{$0.isActive = true}
-        
-        containerImageRond.layer.shadowColor = UIColor.black.cgColor
-        containerImageRond.layer.shadowOpacity = 0.4
-        containerImageRond.layer.shadowOffset = .zero
-        containerImageRond.layer.shadowRadius = 6
-        
-        containerImageRond.addSubview(imageRond)
-        
-        imageRond.layer.masksToBounds = true
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
-        
-        containerImageRond.isUserInteractionEnabled = true
-        containerImageRond.addGestureRecognizer(tapGestureRecognizer)
-        
-        containerImageRond.layer.zPosition = 1
-        imageRond.layer.zPosition = 1
-
-        imageRond.translatesAutoresizingMaskIntoConstraints = false
-        imageRond.layer.cornerRadius = 45
-        
-
-        [
-            imageRond.widthAnchor.constraint(equalToConstant: 90),
-            imageRond.leftAnchor.constraint(equalTo: containerImageRond.leftAnchor, constant: 0),
-            imageRond.heightAnchor.constraint(equalToConstant: 90),
-            imageRond.topAnchor.constraint(equalTo: containerImageRond.topAnchor, constant: 0)
-        ].forEach{$0.isActive = true}
-
-        // Image player
-        
-        
-        playerOnImage.image = Assets.Picto.play.image
-        imageRond.addSubview(playerOnImage)
-
-        playerOnImage.translatesAutoresizingMaskIntoConstraints = false
-        [
-            playerOnImage.widthAnchor.constraint(equalToConstant: 30),
-            playerOnImage.centerXAnchor.constraint(equalTo: imageRond.centerXAnchor),
-            playerOnImage.heightAnchor.constraint(equalToConstant: 30),
-            playerOnImage.centerYAnchor.constraint(equalTo: imageRond.centerYAnchor),
-        ].forEach{$0.isActive = true}
- 
-        
-        text.backgroundColor = .clear
-        text.axis = .vertical
-        text.distribution = .fill
-        text.alignment = .fill
-
-        title.text = "EPISODE 82"
-        title.frame.size.height = 20
-        title.font = UIFont(name: .fonts.proximaNova_Thin.fontName(), size: 19)
-        title.textColor = Colors.darkBlue.color
-
-        line.frame.size.height = 3
-        line.backgroundColor = Colors.darkBlue.color
-
-       
-        subtitle.text = "Cuisines indiennes, clichés en sauce"
-        subtitle.font = UIFont(name: .fonts.proximaNova_Thin.fontName(), size: 14)
-        subtitle.textColor = Colors.darkBlue.color
-        subtitle.numberOfLines = 2
-
-        text.addArrangedSubview(title)
-        text.addArrangedSubview(line)
-        text.addArrangedSubview(subtitle)
-
-        text.translatesAutoresizingMaskIntoConstraints = false
-
-        addSubview(text)
-        
-        [
-            text.topAnchor.constraint(equalTo: topAnchor, constant: 10),
-            text.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
-            text.leftAnchor.constraint(equalTo: leftAnchor, constant: 105),
-            text.rightAnchor.constraint(equalTo: rightAnchor, constant: -30),
-            line.heightAnchor.constraint(equalToConstant: 1),
-            title.heightAnchor.constraint(equalToConstant: 30)
-        ].forEach{$0.isActive = true}
-     
-    }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer)
-    {
-        print("@@@ tapGestureRecognizer = \(tapGestureRecognizer)")
-        let tappedImage = tapGestureRecognizer.view
-        print("@@@ tapped Image = \(tappedImage)")
-        isActive.toggle()
-        active()
-    }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     func applyDesign() {
         
-        
-        
-
         innerShadowTop.frame = bounds
-
-        // Shadow path (1pt ring around bounds)
         let radius = frame.size.height/2
-
-        let path = UIBezierPath(roundedRect: innerShadowTop.bounds.insetBy(dx: -3, dy: -3), cornerRadius:radius)
-        let cutout = UIBezierPath(roundedRect: innerShadowTop.bounds, cornerRadius:radius).reversing()
-
-        
-
+        let path = UIBezierPath(roundedRect: innerShadowTop.bounds.insetBy(dx: -3, dy: -3), cornerRadius: radius)
+        let cutout = UIBezierPath(roundedRect: innerShadowTop.bounds, cornerRadius: radius).reversing()
         path.append(cutout)
         innerShadowTop.shadowPath = path.cgPath
         innerShadowTop.masksToBounds = true
@@ -215,5 +59,254 @@ class SwitchPlayer: UIView {
         innerShadowTop.cornerRadius = radius
         layer.addSublayer(innerShadowTop)
     }
+
+    let containerCircleImage: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.4
+        view.layer.shadowOffset = .zero
+        view.layer.shadowRadius = 6
+        view.layer.zPosition = 1
+        return view
+    }()
+
+    let imageCircle: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.layer.masksToBounds = true
+        image.layer.cornerRadius = 45
+        image.layer.zPosition = 1
+        return image
+    }()
     
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.frame.size.height = 20
+        label.font = UIFont(name: .fonts.proximaNova_Thin.fontName(), size: 19)
+        label.textColor = Colors.darkBlue.color
+        return label
+    }()
+    
+    let lineSeperator: UIView = {
+        let line = UIView()
+        line.translatesAutoresizingMaskIntoConstraints = false
+        line.frame.size.height = 3
+        line.backgroundColor = Colors.darkBlue.color
+        return line
+    }()
+    
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = UIFont(name: .fonts.proximaNova_Thin.fontName(), size: 14)
+        label.textColor = Colors.darkBlue.color
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    let textStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.backgroundColor = .clear
+        stack.axis = .vertical
+        stack.distribution = .fill
+        stack.alignment = .fill
+        return stack
+    }()
+    
+    let imagePlayer: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: Assets.Picto.playHomePage.name)
+        return image
+    }()
+
+    func setup() {
+
+        // general view
+        backgroundColor = .white
+        layer.cornerRadius = 50
+        /*
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOpacity = 0
+        layer.shadowOffset = CGSize(width: -1, height: 1)
+        layer.shadowRadius = 20
+        */
+  
+
+        // layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+
+        // Add view :
+        titleLabel.text = title
+        subtitleLabel.text = subtitle
+        imageCircle.image = UIImage(named: imageString)
+        
+        addSubview(containerCircleImage)
+        containerCircleImage.addSubview(imageCircle)
+        imageCircle.addSubview(imagePlayer)
+        
+        addSubview(textStack)
+        textStack.addArrangedSubview(titleLabel)
+        textStack.addArrangedSubview(lineSeperator)
+        textStack.addArrangedSubview(subtitleLabel)
+
+        
+        //MARK: - Constraints
+        
+        leftContainerImageCircleViewConstraint = containerCircleImage.leftAnchor.constraint(equalTo: leftAnchor, constant: 7)
+        leftTextStackViewConstraint = textStack.leftAnchor.constraint(equalTo: leftAnchor, constant: 105)
+
+        NSLayoutConstraint.activate([
+            leftContainerImageCircleViewConstraint!,
+            containerCircleImage.topAnchor.constraint(equalTo: topAnchor, constant: 7),
+            containerCircleImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 7),
+            containerCircleImage.widthAnchor.constraint(equalToConstant: 90),
+            imageCircle.widthAnchor.constraint(equalToConstant: 90),
+            imageCircle.leftAnchor.constraint(equalTo: containerCircleImage.leftAnchor, constant: 0),
+            imageCircle.heightAnchor.constraint(equalToConstant: 90),
+            imageCircle.topAnchor.constraint(equalTo: containerCircleImage.topAnchor, constant: 0),
+            imagePlayer.widthAnchor.constraint(equalToConstant: 30),
+            imagePlayer.centerXAnchor.constraint(equalTo: imageCircle.centerXAnchor),
+            imagePlayer.heightAnchor.constraint(equalToConstant: 30),
+            imagePlayer.centerYAnchor.constraint(equalTo: imageCircle.centerYAnchor),
+            textStack.topAnchor.constraint(equalTo: topAnchor, constant: 10),
+            textStack.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10),
+            textStack.widthAnchor.constraint(equalTo: widthAnchor, constant: -130),
+            leftTextStackViewConstraint!,
+            lineSeperator.heightAnchor.constraint(equalToConstant: 1),
+            titleLabel.heightAnchor.constraint(equalToConstant: 30)
+        ])
+
+        
+        
+        // Swipe gesture
+        let panGestureRecognizer = UIPanGestureRecognizer(
+            target: self,
+            action: #selector(self.handlePanGesture(_:))
+        )
+        panGestureRecognizer.minimumNumberOfTouches = 1
+        
+        // Tap gesture
+        let tapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(imageTapped(tapGestureRecognizer:))
+        )
+        
+        containerCircleImage.isUserInteractionEnabled = true
+        containerCircleImage.addGestureRecognizer(panGestureRecognizer)
+        containerCircleImage.addGestureRecognizer(tapGestureRecognizer)
+    }
+
+    private func updateContainerImageCircleXPosition(_ x: CGFloat) {
+        leftContainerImageCircleViewConstraint?.constant = x
+    }
+
+    private func updateTextSkatXPosition(_ x: CGFloat) {
+        self.leftTextStackViewConstraint?.constant = x
+    }
+
+    func reset() {
+        isFinished = false
+        updateContainerImageCircleXPosition(7)
+        titleLabel.attributedText = "3ab apple3 3bana".reduce(NSMutableAttributedString()) {
+            $0.append(
+                NSAttributedString(
+                    string: String($1),
+                    attributes: [
+                        .foregroundColor: $1 == "3" ? UIColor.red : .black
+                    ]
+                )
+            )
+            return $0
+        }
+    }
+
+    func active(action: Action) {
+        if isFinished {
+            UIView.animate(withDuration: 0.8, delay: 0, options: [.curveEaseOut]) {
+                self.updateTextSkatXPosition(self.leftTextMinConstrait)
+                //
+                if action == .tap {
+                    // self.updateContainerImageCircleXPosition(self.xEndingPoint)
+                    self.containerCircleImage.frame.origin.x += UIScreen.main.bounds.width - 70 - 107
+                }
+                self.textStack.frame.origin.x -= 70
+                self.titleLabel.font = UIFont(name: .fonts.proximaNova_Alt_Bold.fontName(), size: 19)
+                self.subtitleLabel.font = UIFont(name: .fonts.proximaNova_Alt_Bold.fontName(), size: 14)
+                self.imagePlayer.image = Assets.Picto.pauseHomePage.image
+                
+                self.layer.shadowOpacity = 0.5
+                self.layer.shadowOffset = CGSize(width: -1, height: 3)
+                self.layer.shadowRadius = 5
+            }
+            self.innerShadowTop.isHidden = true
+        } else {
+            UIView.animate(withDuration: 1) {
+                self.updateTextSkatXPosition(self.leftTextMaxConstrait)
+                self.containerCircleImage.frame.origin.x -= UIScreen.main.bounds.width - 70 - 107
+                self.textStack.frame.origin.x += 70
+                self.innerShadowTop.isHidden = false
+                self.titleLabel.font = UIFont(name: .fonts.proximaNova_Thin.fontName(), size: 19)
+                self.subtitleLabel.font = UIFont(name: .fonts.proximaNova_Thin.fontName(), size: 14)
+                self.imagePlayer.image = Assets.Picto.playHomePage.image
+                
+                self.layer.shadowOpacity = 0
+            }
+            
+            
+            reset()
+        }
+    }
+}
+
+
+enum Action {
+    case tap
+    case swipe
+
+}
+extension SwitchPlayer {
+
+    @objc func imageTapped(tapGestureRecognizer: UITapGestureRecognizer) {
+        isFinished.toggle()
+        active(action: .tap)
+    }
+
+    @objc private func handlePanGesture(_ sender: UIPanGestureRecognizer) {
+        if isFinished { return }
+        let translatedPoint = sender.translation(in: self).x
+
+        switch sender.state {
+        case .changed:
+            if translatedPoint <= 7 {
+                updateContainerImageCircleXPosition(7)
+            } else if translatedPoint >= xEndingPoint {
+                updateContainerImageCircleXPosition(xEndingPoint)
+            } else {
+                updateContainerImageCircleXPosition(translatedPoint)
+                if translatedPoint > 30 {
+                    UIView.animate(withDuration: 1) {
+                        self.textStack.isHidden = true
+                    }
+                }
+            }
+        case .ended:
+            self.textStack.isHidden = false
+            if translatedPoint >= xEndingPoint {
+                self.updateContainerImageCircleXPosition(xEndingPoint)
+                isFinished = true
+                delegate?.didFinish()
+                active(action: .swipe)
+            } else {
+                UIView.animate(withDuration: 1) {
+                    self.reset()
+                }
+            }
+        default:
+            break
+        }
+    }
+
 }
