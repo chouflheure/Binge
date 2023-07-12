017b13dbf1dbd16e90f4e2270083f37588db185a
Charles Calvignac <charles.calvignac@neoxia.com>
2023-07-13 01:05:38 +0200

    add collection view

--- BingePodcast/BingePodcast/Controller/HomeViewController.swift
+++ BingePodcast/BingePodcast/Controller/HomeViewController.swift
@@ -5,9 +5,12 @@ class HomeViewController: UIViewController {
   
     // MARK: - Variables déclaration
     
+    let authorArray = ["Sophie-Marie Larrouy", "Anne-Cécile Genre", "Victoire Tuaillon", "Victoire Tuaillon", "", "Thomas Rozec" ]
+    let titleArray = ["À bientôt de te revoir", "Du Sport", "Le Coeur Sur La Table", "Les couilles sur la table", "Panier Piano", "Programme B"]
+    let cellPodcast = "cellPodcast"
+
     let scrollViewGeeral: UIScrollView = {
         let scrollView = UIScrollView()
-        
         return scrollView
     }()
     
@@ -22,6 +25,7 @@ class HomeViewController: UIViewController {
         let label = UILabel()
         label.text = "À écouter"
         label.font = UIFont(name: .fonts.proximaNova_Alt_Thin.fontName(), size: 22)
+        label.textColor = Colors.darkBlue.color
         return label
     }()
 
@@ -36,22 +40,38 @@ class HomeViewController: UIViewController {
         subtitle: "Cuisines indiennes",
         imageString: Assets.aBientotDeTeRevoir.name
     )
-    
+
     let titlePodcast: UILabel = {
         let label = UILabel()
-        
         label.text = "Nos Podcasts"
         label.font = UIFont(name: .fonts.proximaNova_Alt_Thin.fontName(), size: 22)
+        label.textColor = Colors.darkBlue.color
         return label
     }()
     
-    
-    
+    let myCollectionViewPodcast: UICollectionView = {
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 35, bottom: 0, right: 35)
+        layout.itemSize = CGSize(width: 148, height: 200)
+        layout.minimumLineSpacing = 20
+        layout.scrollDirection = .horizontal
+        
+        let myCollectionViewPodcast = UICollectionView(frame: CGRect(x: 0, y: 30, width: UIScreen.main.bounds.width, height: 200), collectionViewLayout: layout)
+        myCollectionViewPodcast.backgroundColor = .clear
+        myCollectionViewPodcast.isScrollEnabled = true
+        myCollectionViewPodcast.translatesAutoresizingMaskIntoConstraints = false
+        myCollectionViewPodcast.showsVerticalScrollIndicator = false
+        myCollectionViewPodcast.showsHorizontalScrollIndicator = false
+
+        return myCollectionViewPodcast
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        initCollectionView()
         setGradientBackground()
-        
+
         titleSwitchPlayer.translatesAutoresizingMaskIntoConstraints = false
         titlePodcast.translatesAutoresizingMaskIntoConstraints = false
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -104,9 +124,27 @@ class HomeViewController: UIViewController {
             titlePodcast.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -Constants_HomeViewController.horizontalMarginLeftAndRightMolecule),
             titlePodcast.heightAnchor.constraint(equalToConstant: 23)
         ].forEach{$0.isActive = true}
+        
+        
+        view.addSubview(myCollectionViewPodcast)
+         [
+            myCollectionViewPodcast.topAnchor.constraint(equalTo: titlePodcast.bottomAnchor, constant: 10),
+            myCollectionViewPodcast.leftAnchor.constraint(equalTo: view.leftAnchor),
+            myCollectionViewPodcast.rightAnchor.constraint(equalTo: view.rightAnchor),
+            myCollectionViewPodcast.heightAnchor.constraint(equalToConstant: 200),
+
+        ].forEach{$0.isActive = true}
+         
+         
     }
     
-    func setGradientBackground() {
+    private func initCollectionView() {
+        myCollectionViewPodcast.register(PodcastHomePageCollectionViewCell.self, forCellWithReuseIdentifier: cellPodcast)
+        myCollectionViewPodcast.dataSource = self
+        myCollectionViewPodcast.delegate = self
+    }
+    
+    private func setGradientBackground() {
         let colorTop = Colors.ligthBlue.color.cgColor
         let colorBottom = Colors.white.color.cgColor
                     
@@ -117,14 +155,6 @@ class HomeViewController: UIViewController {
                 
         self.view.layer.insertSublayer(gradientLayer, at: 0)
     }
-    
-    // Top image
-    
-    // molécule swipe 1
-    
-    // molécule swipe 2
-    
-    // collection view "Nos podcasts"
 
     private func toggleSelectedSwitchPlayer() {
         
@@ -132,3 +162,22 @@ class HomeViewController: UIViewController {
 
 }
 
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellPodcast, for: indexPath) as? PodcastHomePageCollectionViewCell
+        guard let myCell = myCell else {return UICollectionViewCell()}
+        myCell.setup(imagePodcastName:Assets.aBientotDeTeRevoir.name, titlePodcast: titleArray[indexPath.row], authorPodcast: authorArray[indexPath.row])
+        return myCell
+    }
+    
+    
+}
