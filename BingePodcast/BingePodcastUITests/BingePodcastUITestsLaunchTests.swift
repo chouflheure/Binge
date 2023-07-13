c3712caebbc1d57a173098e76cd971da7899b537
Charles Calvignac <charles.calvignac@neoxia.com>
2023-07-13 12:00:10 +0200

    update test

--- BingePodcast/BingePodcastUITests/BingePodcastUITestsLaunchTests.swift
+++ BingePodcast/BingePodcastUITests/BingePodcastUITestsLaunchTests.swift
@@ -3,16 +3,5 @@ import XCTest
 
 final class BingePodcastUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    func testLaunch() throws {
-        XCTAssertTrue(true)
-    }
 }
 
