f3708818117f11a9222eebb080e93c4ddabb7b06
Charles Calvignac <charles.calvignac@neoxia.com>
2023-09-11 15:46:03 +0200

    remove heart

--- BingePodcast/BingePodcast/Controller/Screen/FavoriteScreen/PageListFavorite/PageListFavorite+UITableViewDelegate.swift
+++ BingePodcast/BingePodcast/Controller/Screen/FavoriteScreen/PageListFavorite/PageListFavorite+UITableViewDelegate.swift
@@ -4,15 +4,19 @@ import UIKit
 extension PageListFavorite: UITableViewDelegate {
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let row = indexPath.row
+        let section = indexPath.section
         let mainStoryboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let secondVC = mainStoryboard.instantiateViewController(withIdentifier: "PlayerViewController")
-            as? PlayerViewController
+        let secondVC = mainStoryboard.instantiateViewController(
+            withIdentifier: "PlayerViewController") as? PlayerViewController
 
+        print("@@@ click = \(indexPath)")
         guard let secondVC = secondVC else {return}
         secondVC.isReturnButtonChevronLeft = true
-        secondVC.titlePodcast.text = "test"
-        secondVC.subtitlePodcast.text = "episode 112"
-        secondVC.descriptionPodcast.text = "test description"
+        secondVC.titlePodcast.text = podcastSaved[section].episode[row].title
+        secondVC.subtitlePodcast.text = podcastSaved[section].episode[row].subtitle
+        secondVC.descriptionPodcast.text = podcastSaved[section].episode[row].description
+        secondVC.imageString = podcastSaved[section].episode[row].imageUrl ?? ""
         secondVC.modalPresentationStyle = .custom
         secondVC.transitioningDelegate = self
 
